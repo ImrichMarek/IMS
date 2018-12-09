7a7701bb3cdb92ce98050a0542f4c6e9a39a5592
ImrichMarek <marek.imrich.333@gmail.com>
2018-12-09 23:39:40 +0100

    edit

--- main.cpp
+++ main.cpp
@@ -17,16 +17,24 @@ Store inputMaterial1("vstupny_materia_sustruhu", std::numeric_limits<unsigned lo
 Store inputMaterial2("vstupny_materia_frezy", std::numeric_limits<unsigned long>::max());
 Store product("vystupny_produkt", std::numeric_limits<unsigned long>::max());
 
-const int hodina = 60; //hodina ->60 minut
-const int den = 1440; //hodina ->1440 minut
-const int rok = 525600; //hodina ->525600 minut
+const int minuta = 1; //1 minuta
+const int hodina = 60; //1 hodina ->60 minut
+const int den = 1440; //1 den ->1440 minut
+const int rok = 525600; //1 rok ->525600 minut
 int machFailureCnt = 0;
 bool machFailureSet = false;
 bool workTime = false;
+int latheFailureCnt = 0;
+int cutterFailureCnt = 0;
+int lathe_products = 0;
+int cutter_products = 0;
+int latheALLCnt = 0;
+int cutterALLCnt = 0;
+
 
 class Lathe_failure : public Process {
     void Behavior() {
-        machFailureCnt++;
+        latheFailureCnt++;
         Seize(lathe);
         Wait(Exponential(REPAIR_TIMEOUT));
         Release(lathe);
@@ -35,7 +43,7 @@ class Lathe_failure : public Process {
 
 class Cutter_failure : public Process {
     void Behavior() {
-        machFailureCnt++;
+        cutterFailureCnt++;
         Seize(cutter);
         Wait(Exponential(REPAIR_TIMEOUT));
         Release(cutter);
@@ -46,8 +54,6 @@ class Linkk : public Process {
     double startTime;
     double endTime;
     public : int workst;
-    public : int lathe_products;
-    public : int cutter_products;
     void Behavior() {
         startTime = Time;
         if (workst == 1) {
@@ -60,36 +66,68 @@ class Linkk : public Process {
     
     void Lathe() {
         if(workTime) { //ak neskoncila pracovna doba
+            latheALLCnt++;
             bool failure = false;
             Leave(inputMaterial1, LATHE_CAPACITY);
             for(int i = 0; i < 3; i++) {
                 double d = Random(); //1% -> chybny kus
-                if (d < 0.01 || lathe_products == LATHE_HARD_REPAIR) { //nastala chyba
+                if (((latheALLCnt % LATHE_HARD_REPAIR) == 0) && (latheALLCnt != 0)) {
+                    (new Lathe_failure)->Activate();
+                    failure = true;
+                    break;
+                }
+                else if (d <= 0.01) { //nastala chyba
+                    (new Lathe_failure)->Activate();
+                    failure = true;
+                    break;
+                }
+                double e = Random();
+                double c = ((LATHE_ROTATIONS_NB/minuta)*(LATHE_HARD_REPAIR/PIECES_NB)/100);
+                if (e <= c) {
                     (new Lathe_failure)->Activate();
                     failure = true;
                 }
             }
             if (!failure) {
                 lathe_products++;
-                Enter(inputMaterial2, 1);
+                Enter(inputMaterial2, LATHE_CAPACITY);
+                if (((inputMaterial2.Used() % CUTTER_CAPACITY) == 0) && (inputMaterial2.Used() != 0)) {
+                    Linkk* lnk = new Linkk;
+                    lnk->workst = 2; //start cutter
+                    lnk->Activate();
+                }
             }
         }
     }
 
     void Cutter() {
         if(workTime) { //ak neskoncila pracovna doba
+            cutterALLCnt++;
             bool failure = false;
             Leave(inputMaterial2, CUTTER_CAPACITY);
             for(int i = 0; i < 2; i++) {
                 double d = Random(); //1% -> chybny kus
-                if (d < 0.01 || lathe_products == CUTTER_HARD_REPAIR) { //nastala chyba
+                if (((cutterALLCnt % LATHE_HARD_REPAIR) == 0) && (cutterALLCnt != 0)) {
+                    (new Lathe_failure)->Activate();
+                    failure = true;
+                    break;
+                }
+                else if (d < 0.01) { //nastala chyba
                     (new Cutter_failure)->Activate();
                     failure = true;
+                    break;
+                }
+                double e = Random();
+                double c = ((CUTTER_ROTATIONS_NB/minuta)*(CUTTER_HARD_REPAIR/CUTTER_CAPACITY)/100);
+                if (e <= c) {
+                    (new Cutter_failure)->Activate();
+                    failure = true;
+                    break;
                 }
             }
             if (!failure) {
                 cutter_products++;
-                Enter(product, 12);
+                Enter(product, CUTTER_CAPACITY);
             }
         }
     }
@@ -121,26 +159,48 @@ class Day_cycle : public Event {
 class Link_generator : public Event {
     Linkk* lnk;
     void Behavior() {
-        for (int i = 0; i < (LATHE_NB); i++) {
+        for (int i = 0; i < PIECES_NB; i++) {
             lnk = new Linkk;
             lnk->workst = 1; //start lathe
-            lnk->Activate();
-        }
-        
-        for (int i = 0; i < (CUTTER_NB); i++) {
-            lnk = new Linkk;
-            lnk->workst = 1; //start cutter
             lnk->Activate();
         }
     }
 };
 
 int main() {
-    Init(0, SIM_TIME); //cas simulacie 0..SIM_TIME
+    Init(0, SIM_TIME*hodina); //cas simulacie 0..SIM_TIME
     (new Day_cycle)->Activate();
     (new Link_generator)->Activate();
     
 	Run();
+    
+    std::cout<<"/***********************************************************************/\n\n";
+    std::cout<<"/**************************  Vstupy simulácie  *************************/\n\n";
+    std::cout<<"/***********************************************************************/\n\n";
+    std::cout<<"Celkový čas simulácie bol nastavený na: "<<SIM_TIME<<" hodín.\n";
+    std::cout<<"Pracovná doba bola nastavená na: "<<WORK_TIME<<" hodín.\n";
+    std::cout<<"Pracovná doba bola nastavená na: "<<NON_WORK_TIME<<" hodín.\n";
+    std::cout<<"Čas opravy stroja po chybe exp() bol nastavený na: "<<REPAIR_TIMEOUT<<" hodín.\n\n";
+    
+    std::cout<<"Celkový počet vstupného materálu bol nastavený na "<<PIECES_NB<<" ks.\n\n";
+    
+    std::cout<<"Kapacita sústruhu bola nastavená na: "<<LATHE_CAPACITY<<" ks materíalu.\n";
+    std::cout<<"Otáčky sústruhov boli nastavený na: "<<LATHE_ROTATIONS_NB<<" ot/min.\n";
+    std::cout<<"Nútená výmena vrtáku u sústruhov bola nastavená po opracovaní: "<<LATHE_HARD_REPAIR<<" ks.\n\n";
+    
+    std::cout<<"Kapacita fréz bola nastavená na: "<<CUTTER_CAPACITY<<" ks materíalu.\n";
+    std::cout<<"Otáčky fréz boli nastavený na: "<<CUTTER_ROTATIONS_NB<<" ot/min.\n";
+    std::cout<<"Nútená výmena vrtáku u frézy bola nastavená po opracovaní: "<<CUTTER_HARD_REPAIR<<" ks.\n\n\n\n";
 
+    std::cout<<"/***********************************************************************/\n\n";
+    std::cout<<"/*************************  Výstupy simulácie  *************************/\n\n";
+    std::cout<<"/***********************************************************************/\n\n";
+    
+    std::cout<<"Počet kusov, ktore vyrobil sústruh bol: "<<lathe_products<<" ks.\n";
+    std::cout<<"Počet chýb, kedy zlyhanie zapríčinil sústruh bolo: "<<latheFailureCnt<<".\n";
+    
+    std::cout<<"Počet kusov, ktore vyrobila fréza bol: "<<cutter_products*CUTTER_CAPACITY<<".\n";
+    std::cout<<"Počet chýb, kedy zlyhanie zapríčinila fréza bolo: "<<cutterFailureCnt*CUTTER_CAPACITY<<".\n";
+    
 	return 0;
 }
--- main.h
+++ main.h
@@ -12,22 +12,18 @@
 #include <iostream>
 #include <limits>
 
-#define SIM_TIME 1000		    //cas simulacie
+#define SIM_TIME 10000000	    //cas simulacie v hodinach
 #define WORK_TIME 8             //cas pracovnej doby v hodinach
 #define NON_WORK_TIME 16        //cas mimopracovnej doby v hodinach
-#define RANDOM_FAILURE_TIME 240 //cas vyskytu nahodnej chyby exp()
 #define REPAIR_TIMEOUT 5        //čas opravy po chybe
 
-#define LATHE_NB 2              //pocet sustruhov
 #define LATHE_CAPACITY 1        //kapacita prveho stroja
 #define LATHE_ROTATIONS_NB 1    //pocet otacok
 #define LATHE_HARD_REPAIR 270   //pocet ks, kedy je vynutena vymena
 
-
-#define CUTTER_NB 1             //pocet frez
 #define CUTTER_CAPACITY 12      //kapacita druheho stroja
 #define CUTTER_ROTATIONS_NB 1   //pocet otacok
 #define CUTTER_HARD_REPAIR 900  //pocet ks, kedy je vynutena vymena
 
-#define PIECES_NB 100           //pocet kusov
+#define PIECES_NB 1000          //pocet kusov materiálu na vstupe
 
